26e312677cbb4b8cd23ddb9fa1a9d2b5fcae6a93
IgorSilvaZZ <igor06silva12@gmail.com>
2022-08-02 22:42:55 -0300

    Create client StormGlass

---
+++ src/clients/__test__/stomGlass.test.ts
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+import { StormGlass } from '@src/clients/StormGlass';
+import stormGlassWeather3HoursFixture from '@test/fixtures/stormglass_weather_3_hours.json';
+import stormGlassNormalized3HoursFixture from '@test/fixtures/stormglass_normalized_response_3_hours.json';
+
+// Tudo que for do axios vai ser mockado
+jest.mock('axios');
+
+describe('StormGlass client', () => {
+  it('Should return the normalized forecast frm the StormGlass service', async () => {
+    const lat = -33.792726;
+    const lgn = 151.289824;
+
+    // Criando um mock de uma requisição GET do axios, e passado o retorno desse request.
+    axios.get = jest.fn().mockResolvedValue(stormGlassWeather3HoursFixture);
+
+    const stormGlass = new StormGlass(axios);
+
+    const response = await stormGlass.fetchPoints(lat, lgn);
+
+    expect(response).toEqual(stormGlassNormalized3HoursFixture);
+  });
+});
