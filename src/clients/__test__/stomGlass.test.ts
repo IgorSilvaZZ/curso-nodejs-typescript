90f2260b962eb325f77edb8cd835f773dc77c4f0
IgorSilvaZZ <igor06silva12@gmail.com>
2022-08-03 21:39:21 -0300

    Add types in mocks jest

--- src/clients/__test__/stomGlass.test.ts
+++ src/clients/__test__/stomGlass.test.ts
@@ -8,16 +8,18 @@ import stormGlassNormalized3HoursFixture from '@test/fixtures/stormglass_normali
 jest.mock('axios');
 
 describe('StormGlass client', () => {
+  // Adicionando os tipos de axios no mocked do jest
+  // Assim conseguindo utilizar os dois tipos dentro dessa variavel
+  const mockedAxios = axios as jest.Mocked<typeof axios>;
+
   it('Should return the normalized forecast frm the StormGlass service', async () => {
     const lat = -33.792726;
     const lgn = 151.289824;
 
     // Criando um mock de uma requisição GET do axios, e passado o retorno desse request.
-    axios.get = jest
-      .fn()
-      .mockResolvedValue({ data: stormGlassWeather3HoursFixture });
+    mockedAxios.get.mockResolvedValue({ data: stormGlassWeather3HoursFixture });
 
-    const stormGlass = new StormGlass(axios);
+    const stormGlass = new StormGlass(mockedAxios);
 
     const response = await stormGlass.fetchPoints(lat, lgn);
 
